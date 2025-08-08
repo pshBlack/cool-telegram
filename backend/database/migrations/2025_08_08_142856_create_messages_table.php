20599ed7fe75f596ab323a46ae1798def44e9fef
Vlad <ribakvlad@gmail.com>
2025-08-08 19:36:38 +0200

    dd

---
+++ backend/database/migrations/2025_08_08_142856_create_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chat_id')->constrained('chats')->onDelete('cascade');
+            $table->foreignId('sender_id')->nullable()->constrained('users1')->onDelete('set null');
+            $table->text('content')->nullable();
+            $table->text('media_url')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('edited_at')->nullable();
+            $table->boolean('is_deleted')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
